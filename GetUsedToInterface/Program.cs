b7df86b917d5b27f2401d2ffb781e569487ad594
Phat Tran <ptran@strongtie.com>
2024-01-13 12:50:50 +0700

    Add project files.

---
+++ GetUsedToInterface/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GetUsedToInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            tallGuy.TalkAboutYourself();
+        }
+    }
+}
---
+++ GetUsedToInterface/TallGuy.cs
@@ -0,0 +1,13 @@
+﻿namespace GetUsedToInterface
+{
+    public class TallGuy
+    {
+        public string Name;
+        public int Height;
+
+        public void TalkAboutYourself()
+        {
+            Console.WriteLine($"My name is {Name} and I'm {Height} inches tall.");
+        }
+    }
+}
