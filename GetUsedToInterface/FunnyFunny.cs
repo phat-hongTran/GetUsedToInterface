02925f7aaed76779419a46bde652f6db249be610
Phat Tran <ptran@strongtie.com>
2024-01-14 15:08:54 +0700

    Finish

---
+++ GetUsedToInterface/FunnyFunny.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GetUsedToInterface
+{
+    class FunnyFunny : IClown
+    {
+        private string funnyThingIHave;
+        public string FunnyThingIHave { get { return funnyThingIHave; } }
+
+        public void Honk()
+        {
+            Console.WriteLine("Hi kids! I have a " + FunnyThingIHave);
+        }
+
+        public FunnyFunny(string funnyThing)
+        {
+            funnyThingIHave = funnyThing;
+        }
+    }
+}
--- GetUsedToInterface/Program.cs
+++ GetUsedToInterface/Program.cs
@@ -6,10 +6,17 @@ namespace GetUsedToInterface
     {
         static void Main(string[] args)
         {
-            TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
-            tallGuy.TalkAboutYourself();
-            Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
-            tallGuy.Honk();
+            //TallGuy tallGuy = new TallGuy() { Height = 76, Name = "Jimmy" };
+            //tallGuy.TalkAboutYourself();
+            //Console.WriteLine($"The tall guy has {tallGuy.FunnyThingIHave}");
+            //tallGuy.Honk();
+
+            IClown fingersTheClown = new ScaryScary("big red nose", 14);
+            fingersTheClown.Honk();
+            if (fingersTheClown is IScaryClown iScaryClownReference)
+            {
+                iScaryClownReference.ScareLittleChildren();
+            }
         }
     }
 }
